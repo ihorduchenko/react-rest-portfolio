91cf0f625ccb3f5954bca9643196957c3c0f2276
Ihor Duchenko <ihor.duchenko.777@gmail.com>
2019-07-24 23:53:29 +0300

    Fixing redux error

--- src/store.js
+++ src/store.js
@@ -6,19 +6,14 @@ const initialState = {};
 
 const middleware = [thunk];
 
-let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__();
-
-if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
-  devTools = a => a;
-}
-
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    devTools
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
