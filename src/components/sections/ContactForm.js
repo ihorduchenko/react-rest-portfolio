43d9dad3ca0b573c329673ddb5141c3ec14d3916
Ihor Duchenko <igordu4enko@ukr.net>
2019-07-24 18:59:08 +0300

    Sending email using EmailJS

--- src/components/sections/ContactForm.js
+++ src/components/sections/ContactForm.js
@@ -1,44 +1,87 @@
 import React, { Component } from "react";
-import { MDBBtn, MDBInput } from 'mdbreact';
+import * as emailjs from "emailjs-com";
+import { MDBBtn, MDBInput } from "mdbreact";
+
+import { isEmpty } from '../../helpers';
 
 class ContactForm extends Component {
-  componentDidMount() {
-    
+  state = {
+    your_name: '',
+    your_email: '',
+    where_found: '',
+    message: ''
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { your_name, your_email, where_found, message } = this.state;
+
+    let templateParams = {
+      "your_name": your_name,
+      "your_email": your_email,
+      "where_found": where_found,
+      "message": message
+    };
+
+    let service_id = "default_service";
+    let template_id = "template_9g3yiL3l";
+    if(!isEmpty(your_name) && !isEmpty(your_email) && !isEmpty(message)) {
+      emailjs.send(service_id, template_id, templateParams, 'user_QPtlGIKYiW75zbGhJq94s');
+      this.resetForm();
+    } else {
+      alert('Please, fill in required fields!');
+    }
   }
+
+  resetForm() {
+    this.setState({
+      your_name: '',
+      your_email: '',
+      where_found: '',
+      message: ''
+    });
+  }
+
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value });
+  };
+
+  componentDidMount() {}
 
   render() {
     return (
-      <form className="mt-5">
+      <form className="mt-5" onSubmit={this.handleSubmit.bind(this)}>
         <div className="grey-text">
           <MDBInput
             name="your_name"
-            label="Your name"
+            value={this.state.your_name}
+            onChange={this.handleChange.bind(this, 'your_name')}
+            label="Your name *"
             type="text"
             validate
-            error="wrong"
-            success="right"
           />
           <MDBInput
             name="your_email"
-            label="Your email"
+            value={this.state.your_email}
+            onChange={this.handleChange.bind(this, 'your_email')}
+            label="Your email *"
             type="email"
             validate
-            error="wrong"
-            success="right"
           />
           <MDBInput
             name="where_found"
+            value={this.state.where_found}
+            onChange={this.handleChange.bind(this, 'where_found')}
             label="Where did you find me?"
             type="text"
-            validate
-            error="wrong"
-            success="right"
           />
           <MDBInput
             name="message"
+            value={this.state.message}
+            onChange={this.handleChange.bind(this, 'message')}
             type="textarea"
             rows="2"
-            label="Your message"
+            label="Your message *"
           />
         </div>
         <div className="text-center">
@@ -49,6 +92,6 @@ class ContactForm extends Component {
       </form>
     );
   }
-};
+}
 
 export default ContactForm;
--- src/helpers.js
+++ src/helpers.js
@@ -14,3 +14,9 @@ export const detectBtnStyle = color => {
         return '';
   }
 };
+
+export const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
