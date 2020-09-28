165310993c817f847cef645093dedc13bd109d1e
Ihor Duchenko <igordu4enko@ukr.net>
2020-09-28 21:51:29 +0300

    Trying to deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,24 @@ import SocialIcons from './common/SocialIcons';
 class Footer extends Component { 
   render() {
     const { options } = this.props.options;
-    let footerSlogan = options.acf.footer_section.slogan;
-    let footerSocials = options.acf.socials_fields;
     console.log('foote options: ');
     console.log(options);
+    let footerSlogan = options.acf.footer_section.slogan;
+    let footerSocials = options.acf.socials_fields;
     console.log('footerSlogan: ');
     console.log(footerSlogan);
     console.log('footerSocials: ');
     console.log(footerSocials);
     return (
-      <footer className="py-5 mt-auto">
-        <div className="container text-center">
-          <h2>{ footerSlogan ? footerSlogan : 'no sloan' }</h2>
-          { footerSocials ? <SocialIcons socials={footerSocials} /> : 'no socials' }
-          <div className="mt-3">
-            © {new Date().getFullYear()} - Ihor Duchenko
-          </div>
-        </div>
-      </footer>    
+      // <footer className="py-5 mt-auto">
+      //   <div className="container text-center">
+      //     <h2>{ footerSlogan ? footerSlogan : 'no sloan' }</h2>
+      //     { footerSocials ? <SocialIcons socials={footerSocials} /> : 'no socials' }
+      //     <div className="mt-3">
+      //       © {new Date().getFullYear()} - Ihor Duchenko
+      //     </div>
+      //   </div>
+      // </footer>    
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,43 +15,44 @@ class Header extends Component {
 
   render() {
     const { options } = this.props.options;
-    let logoSVGcode = options.acf.graphics_fields.logo_svg_code;
-    let logo = logoSVGcode ? <span className="logo-svg" dangerouslySetInnerHTML={{ __html: logoSVGcode }} /> : '';
     console.log('heade options: ');
     console.log(options);
+    let logoSVGcode = options.acf.graphics_fields.logo_svg_code;
+    let logo = logoSVGcode ? <span className="logo-svg" dangerouslySetInnerHTML={{ __html: logoSVGcode }} /> : '';
+    
     console.log('logoSVGcode: ');
     console.log(logoSVGcode);
 
     return (
-      <MDBNavbar color="indigo" dark expand="md" scrolling fixed="top" tag="header">
-        <MDBContainer>
-          <MDBNavbarBrand className="py-0">
-            <NavLink exact className="nav-link white-text p-0" to="/">{ logo }</NavLink>
-          </MDBNavbarBrand>
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
-          <MDBCollapse isOpen={this.state.isOpen} navbar>
-            <MDBNavbarNav className="mt-2 mt-md-0 align-items-md-center" right>
-              <MDBNavItem>
-                <NavLink exact className="nav-link font-weight-normal" to={'/'}>Portfolio</NavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <NavLink exact className="nav-link font-weight-normal" to={'/about'}>About</NavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <NavLink exact className="nav-link font-weight-normal" to={'/blog'}>Blog</NavLink>
-              </MDBNavItem>
-              { options.acf.common_links.auth_link && 
-                <MDBNavItem className="ml-md-3">
-                  <ContentButton classNames="btn btn-info btn-sm" link={options.acf.common_links.auth_link} />
-                </MDBNavItem>
-              }
-              <MDBNavItem className="ml-md-3">
-                <NavLink exact className="btn btn-primary btn-sm" to={'/contact'}>Contact</NavLink>
-              </MDBNavItem>
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBContainer>
-      </MDBNavbar>
+      // <MDBNavbar color="indigo" dark expand="md" scrolling fixed="top" tag="header">
+      //   <MDBContainer>
+      //     <MDBNavbarBrand className="py-0">
+      //       <NavLink exact className="nav-link white-text p-0" to="/">{ logo }</NavLink>
+      //     </MDBNavbarBrand>
+      //     <MDBNavbarToggler onClick={this.toggleCollapse} />
+      //     <MDBCollapse isOpen={this.state.isOpen} navbar>
+      //       <MDBNavbarNav className="mt-2 mt-md-0 align-items-md-center" right>
+      //         <MDBNavItem>
+      //           <NavLink exact className="nav-link font-weight-normal" to={'/'}>Portfolio</NavLink>
+      //         </MDBNavItem>
+      //         <MDBNavItem>
+      //           <NavLink exact className="nav-link font-weight-normal" to={'/about'}>About</NavLink>
+      //         </MDBNavItem>
+      //         <MDBNavItem>
+      //           <NavLink exact className="nav-link font-weight-normal" to={'/blog'}>Blog</NavLink>
+      //         </MDBNavItem>
+      //         { options.acf.common_links.auth_link && 
+      //           <MDBNavItem className="ml-md-3">
+      //             <ContentButton classNames="btn btn-info btn-sm" link={options.acf.common_links.auth_link} />
+      //           </MDBNavItem>
+      //         }
+      //         <MDBNavItem className="ml-md-3">
+      //           <NavLink exact className="btn btn-primary btn-sm" to={'/contact'}>Contact</NavLink>
+      //         </MDBNavItem>
+      //       </MDBNavbarNav>
+      //     </MDBCollapse>
+      //   </MDBContainer>
+      // </MDBNavbar>
     );
   }
 }
